183e9a9ec4dc026687fb9c54c2973064e180c411
Nguyễn Hữu Quỳnh <quynhvpit@outlook.com>
2025-05-14 20:31:00 +0700

    update acc

--- auto.py
+++ auto.py
@@ -3,23 +3,85 @@ import time
 from datetime import datetime
 
 # ✅ Đọc danh sách mã giftcode từ file
-# with open("gift_use_one.txt", "r", encoding="utf-8") as f:
-#     giftcodes = [line.strip() for line in f if line.strip()]
-with open("gift.txt", "r", encoding="utf-8") as f:
+with open("gift_use_one.txt", "r", encoding="utf-8") as f:
     giftcodes = [line.strip() for line in f if line.strip()]
+# with open("gift.txt", "r", encoding="utf-8") as f:
+#     giftcodes = [line.strip() for line in f if line.strip()]
 
 # ✅ Thông tin người dùng (cần thay bằng của bạn)
-# acc = "nguyenhuuquynh2008@gmail.com "
-# user_info = {"userId":"3201572979494952960","profileId":"304cb6b0afe06981","serverId":"61","gameCode":"946","roleId":"1801507457928572961","roleName":"KidS","level":"" }
-# # deadheartanytime@gmail.com 
-# user_info = {"userId":"3204534136623620096","profileId":"33c5fbb541a5c981","serverId":"30","gameCode":"946","roleId":"1801473370581489933","roleName":"oSloW","level":""}
-# acc = "quynh.nguyenhuu@imipgroup.com"
-# user_info = {"userId":"3206406653746823168","profileId":"30e66b2b8ef88981","serverId":"36","gameCode":"946","roleId":"1801479968108685418","roleName":"slow36e","level":""}
-acc = "nguyenquynhvp.ictu@gmail.com"
 link = "https://giftcode.vnggames.com/vn/redeem/946"
-user_info = {"userId":"3216562732197855232","profileId":"332f45f0f761e981","serverId":"89","gameCode":"946","roleId":"1801538246766174210","roleName":"snow1","level":"","code":"12321"}
+# account start
+manageAcc = {
+    "comebackvs1": {
+        "info": {
+            "userId": "3204549693749542912",
+            "profileId": "33c5f2a0ab65c981",
+            "serverId": "93",
+            "gameCode": "946",
+            "roleId": "1801473370585278761",
+            "roleName": "slow123",
+            "level": "",
+            "code": "MUSTREAM"
+        },
+        "auth": "R1h6NTZJRWZoaWZnNHZobkJiUXpCWVRGRW9TT0lUM0NjVlc1K0Y5VmoxUT1FIVYzb3NCb2ZoX0std01aWlRVMVJTZCp3ITNFREFfVFk4ZkxRR1VnMXZiSHNUS2dEeWNEYTdkSi0tNi1yRzVFWHpqV3Ejc24obC0yQVUxdXdMdE8rMTM3MjE5NTczOTg3NzU2ODUxMg=="
+    },
+    "comebackvs2": {
+        "info": "",
+        "auth": ""
+    },
+    "comebackvs3": {
+        "info": "",
+        "auth": ""
+    },
+    "comebackvs4": {
+        "info": "",
+        "auth": ""
+    },
+    "comebackvs5": {
+        "info": "",
+        "auth": ""
+    },
+    "comebackvs6": {
+        "info": "",
+        "auth": ""
+    },
+    "comebackvs7": {
+        "info": {
+            "userId": "3217197265211113472",
+            "profileId": "332c7a3392e1e981",
+            "serverId": "93",
+            "gameCode": "946",
+            "roleId": "1801542645300502569",
+            "roleName": "slowa",
+            "level": "",
+            "code": "4234"
+        },
+        "auth": "d3ZCMHNRaWVnYjU1aE5ld1JwMmc2YkNpSk8xeXRKRTNCK1lCTG5ianllcz1ocikjYVRGR09SUzFVOUxTYnhabG9Wa1AoQ1YwU01ieCpTbnc1a1poUXN1Qy0za1E0d01lV0MhNE5CM3kzUkN1X2kpWlNHQF8xelhzYWdpcUxJd0ErMTM3MTgzODY3Njg3Njc5NTkwNA=="
+    },
+    "comebackvs8": {
+        "info": "",
+        "auth": ""
+    },
+    "comebackvs9": {
+        "info": "",
+        "auth": ""
+    },
+    "comebackvs10": {
+        "info": "",
+        "auth": ""
+    }
+}
+
+# account end
+
+accName = "comebackvs1"
+acc = manageAcc[accName] # type: ignore
+user_info = acc["info"]
+# user_info["serverId"] = "" # server thứ : vd: 93
+# user_info["roleName"] = "" # tên nv
+
 # ✅ Token lấy từ trình duyệt
-AUTH_TOKEN = "VXp5MEFMZEEzTXB2MnF1aHN4NmtVam92cEJyMUxONDhlaVo0U0t3ajBUbz0odCpkd3R1RChEdUBOTUw2bioyalMjZGJYZmxmKkxkQExGVUZwT0JOUTZHRFptMyFvRXEhQUFRVTA4RHZ6T0QqVHJ0OUtLdjFmRVFQa0tEIU54VjErMTM2OTIzNzIxMjYzODI0MDc2OA=="
+AUTH_TOKEN = acc["auth"]
 
 
 # Token lay tu file
@@ -66,3 +128,5 @@ with open("result.txt", "w", encoding="utf-8") as result_file:
         print(log)
         result_file.write(log + "\n")
         time.sleep(1)  # Đợi 1 giây để tránh spam quá nhanh
+
+
