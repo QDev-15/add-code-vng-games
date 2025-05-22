232b405be149f9f2e22f30b198c1ac06bc6231d9
quynhvp90 <quynh.nguyenhuu@imipgroup.com>
2025-05-22 17:02:00 +0700

    update account name

--- auto.py
+++ auto.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # ✅ Đọc danh sách mã giftcode từ file
 # with open("gift_use_one.txt", "r", encoding="utf-8") as f:
 #     giftcodes = [line.strip() for line in f if line.strip()]
-with open("gift1.txt", "r", encoding="utf-8") as f:
+with open("gift.txt", "r", encoding="utf-8") as f:
     giftcodes = [line.strip() for line in f if line.strip()]
 
 # ✅ Thông tin người dùng (cần thay bằng của bạn)
@@ -13,65 +13,50 @@ link = "https://giftcode.vnggames.com/vn/redeem/946"
 # account start
 manageAcc = {
     "comebackvs1": {
-        "info": {"userId":"3216562732197855232","profileId":"332f45f0f761e981","serverId":"105","gameCode":"946","roleId":"1801555841641418868","roleName":"kids1","level":"","code":"13123"},
+        "info": {"userId":"3216562732197855232","profileId":"332f45f0f761e981","serverId":"106","gameCode":"946","roleId":"1801556941251940444","roleName":"kid1","level":"","code":"34234"},
         "auth": "WGVIQW9ydlJ2Um5QUlRDZ0p0RkIyaFJxRWlvcmdPL1BZLzBFbkFXVmdSTT0yVnJDcU5fZWdEKWpIODkoYUdSSzZtUkVyKHg2SyFnT29DSnp5VVN4NzVwWTFIUGlzKGpDZE9JZ2J1NDhVczZDeClUaDVJTmNXZzBHN0N6b2xCOUArMTM3NDc0NDI2MTg2NjE5Mjg5Ng=="
     },
     "comebackvs2": {
-        "info": {"userId":"3217190153209102336","profileId":"332c676f83250981","serverId":"105","gameCode":"946","roleId":"1801555841644980326","roleName":"kid","level":"","code":"12332"},
+        "info": {"userId":"3217190153209102336","profileId":"332c676f83250981","serverId":"106","gameCode":"946","roleId":"1801556941252034613","roleName":"kid2","level":"","code":"42342"},
         "auth": "eEd6dTN2RnB1SGd6VUdtMmFMWmlJbDVUWkZERnFObGRQcnVlaEt2cWExQT1IMzVAMnAqJGtQc3Y4cEMzMm8xSzMoUyNreFY3ME1uZ09zMktXYzhQOU1POUkpZWhDcGc3UCREdndyOVJGNksoak8qIVYzamI5eXNjNkhoVlM2RWgrMTM3NDc1MTU1NTg3MjY4MTk4NA=="
     },
     "comebackvs3": {
-        "info": {"userId":"3217192254024466432","profileId":"332c647473e50981","serverId":"105","gameCode":"946","roleId":"1801555841648783551","roleName":"kidss","level":"","code":"1231232"},
+        "info": {"userId":"3217192254024466432","profileId":"332c647473e50981","serverId":"106","gameCode":"946","roleId":"1801556941252118940","roleName":"kid3","level":"","code":"123123"},
         "auth": "ZTVvbVp2TG80eEhSTlc3ZkEzcFBmTnBNQk5WODBtK1dJRjVTSElaNzZxQT1ob1NsY25QMDJDV1BEUHZwJEJwMGlNWW5aSCptRHVoTFpnR1owQGskRWR5c3diNl8kbnNuclIpU1BMY3lLNDkjRlJsSTZYbWlETkdNbVNRUVhNYTUrMTM3NDc1OTM2ODU1NjYwNTQ0MA=="
     },
     "comebackvs4": {
-        "info": {"userId":"3236067817373089792","profileId":"330dff992962f981","serverId":"105","gameCode":"946","roleId":"1801555841652170777","roleName":"kide","level":"","code":"123123"},
+        "info": {"userId":"3236067817373089792","profileId":"330dff992962f981","serverId":"106","gameCode":"946","roleId":"1801556941252243715","roleName":"kid4","level":"","code":"1321"},
         "auth": "SWpJeXhNc1ZaUU42Q2dtSHVRajAwV0dheC9KNFRscGx3Z2xJcS9vRi9paz1vRi1aT3VRQS1WbUA2NTl5WCkwQjl0VGFObmcjVSFaKTJQeEx2YTIzaUBQdG9qbmoyOU9JKTZlcDl0cTlNVDhJeVBaNFZzWGNMayozOGJHNW5YNFArMTM3NDc2NjM0MDM2MzY4MTc5Mg=="
     },
     "comebackvs5": {
-        "info": {"userId":"3217195281360044032","profileId":"332c65d48da1e981","serverId":"105","gameCode":"946","roleId":"1801555841654669385","roleName":"kid5","level":"","code":"q^ưeqwe"},
+        "info": {"userId":"3217195281360044032","profileId":"332c65d48da1e981","serverId":"106","gameCode":"946","roleId":"1801556941252381209","roleName":"kid5","level":"","code":"123213"},
         "auth": "N0tIaVUwKzZHcXFFeStMSTZzaC9KcHpSa1JRYm9xWndidVBUTUpwV1FqZz1iNm5DUHMxWF9Uc3lMJDQ2MU1QRzlZWmxLb1hkcjRfclpSMHBHRE5pdWhzVlhlekFPVl8hX0cjLVVDaHhfVlNpWnRaZ0ZQRDBvRG4qaTlSYnhCQzUrMTM3NDc3MTMwNjkwNjE1NzA1Ng=="
     },
     "comebackvs6": {
-        "info": {"userId":"3209577257987686400","profileId":"311923431b66e981","serverId":"105","gameCode":"946","roleId":"1801555841658179736","roleName":"kidr","level":"","code":"12323"},
+        "info": {"userId":"3209577257987686400","profileId":"311923431b66e981","serverId":"106","gameCode":"946","roleId":"1801556941252495507","roleName":"kid6","level":"","code":"13123"},
         "auth": "akdTak0rVWtGRmhqOVNQRGV6UjZ4Q25zTVdWaDFpWEdZc3RPOFg5clpZST1VbXZMVUo1MFEhKVUzbEA0dUE3cUVrb1JLcClmdVpLeHFIQzhHUTU0d3ppQ2NUOEpBYzdiI2shZTA5b1dPUzNsKVhBNU4tT1F4ZVBsWTYhdW5kNXorMTM3NDc3ODU4NTg5ODU3MzgyNA=="
     },
-    "comebackvs71": {
-        "info": {"userId":"3217197265211113472","profileId":"332c7a3392e1e981","serverId":"105",
-                 "gameCode":"946","roleId":"1801555841639909376",
-                 "roleName":"kids","level":"","code":"12323"},
-        "auth": "bEIzS01KVVpYRkxmaUlwclFtbXp2cWVGa3FQVlM3NTEyeEM2b2x6clNuUT1ONnUoaXVHczNpTUp1RjF0dzAxM0JNeUpKclVqd2RPendfZWx6S21jT2cjeWo4M1Bwejk4eWZXQGphQ3RXbVBsXzQkQTRAT1F1c0tlUnpBblZjemsrMTM3NDc0MTI1MzkxNTQ3MTg3Mg=="
-    },
     "comebackvs7": {
-        "info": {
-            "userId": "3217197265211113472",
-            "profileId": "332c7a3392e1e981",
-            "serverId": "93",
-            "gameCode": "946",
-            "roleId": "1801542645300502569",
-            "roleName": "slowa",
-            "level": "",
-            "code": "4234"
-        },
+        "info": {"userId":"3217197265211113472","profileId":"332c7a3392e1e981","serverId":"106","gameCode":"946","roleId":"1801556941280553448","roleName":"kid7","level":"","code":"123123"},
         "auth": "d3ZCMHNRaWVnYjU1aE5ld1JwMmc2YkNpSk8xeXRKRTNCK1lCTG5ianllcz1ocikjYVRGR09SUzFVOUxTYnhabG9Wa1AoQ1YwU01ieCpTbnc1a1poUXN1Qy0za1E0d01lV0MhNE5CM3kzUkN1X2kpWlNHQF8xelhzYWdpcUxJd0ErMTM3MTgzODY3Njg3Njc5NTkwNA=="
     },
     "comebackvs8": {
-        "info": "",
-        "auth": ""
+        "info": {"userId":"3217200108909740032","profileId":"332c7be68c250981","serverId":"106","gameCode":"946","roleId":"1801556941280688231","roleName":"kid8","level":"","code":"4234234"},
+        "auth": "K2ltK2wzOTVUV2lxaUZMdVd0SHBWQ1RrSXoyb2UySnJuNzFIRHp4anBnbz0oWjFLOVFyQk9DSmlEU3IzI25OdE00WHlEekRFQ0dENDB1VVVzU25FVEVKd3BIX0FJQnQjWVlLQkVhUm5ZVkB2a19wckxCaXNuOVVzOFdRUUk5MSgrMTM3NTAwOTYzMTI0NDE0ODczNg=="
     },
     "comebackvs9": {
-        "info": "",
-        "auth": ""
+        "info": {"userId":"3217203199538905088","profileId":"332c797ebb650981","serverId":"106","gameCode":"946","roleId":"1801556941280850179","roleName":"kid9","level":"","code":"131232"},
+        "auth": "cGxJZHBzaGp1TldqRVFRWkJpQ3lZTklWNG9xV3drT3EwRWh1N3JDb3NmUT12eG5vc1E4IylSbm1haGVfVVh3KUF5RCluUlFQZGprKDZEeGtnblJSTGgqeU04d0BxRjFSMXFIM2R4cHFhbm9XZUBlb0VQMnMpcWVQdjkobm9VdSErMTM3NTAxMDMwODMzODE2NzgwOA=="
     },
-    "comebackvs10": {
-        "info": "",
-        "auth": ""
+    "forever_vp": {
+        "info": {"userId":"3236551504758956032","profileId":"3302dbd997e52981","serverId":"106","gameCode":"946","roleId":"1801556941281970438","roleName":"kid10","level":"","code":"312323"},
+        "auth": "akcyMDhnTlRBcmMzLytiNkNGb3luUTBrd21lRDh5ZkwxRmZTS1NSVVUyRT10YWhRM1lKQFRZUnlBMEF2c0hETXZOMChlNkQjdU9kYUQjY0h3RDJpSDhmcTVNKmdXS1hVJFlZWkQkRHJUalZ5UFBydDkyYmdiWHp2X2ZuLXY0cEkrMTM3NTAwNzYyODE4NjMwMDQxNg=="
     }
 }
 
 # account end
 
-accName = "comebackvs6"
+accName = "comebackvs9"
 acc = manageAcc[accName] # type: ignore
 user_info = acc["info"]
 # user_info["serverId"] = "" # server thứ : vd: 93
@@ -118,12 +103,10 @@ with open("result.txt", "w", encoding="utf-8") as result_file:
             result = response.json()
             # Tùy API, bạn có thể kiểm tra message, statusCode, v.v.
             status_msg = result.get("message") or str(result)
-            log = f"[{code}] → {status_msg}"
+            log = f"[{accName}]:[{code}] → {status_msg}"
         except Exception as e:
-            log = f"[{code}] → LỖI: {e}"
+            log = f"[{accName}]:[{code}] → LỖI: {e}"
 
         print(log)
         result_file.write(log + "\n")
         time.sleep(1)  # Đợi 1 giây để tránh spam quá nhanh
-
-
